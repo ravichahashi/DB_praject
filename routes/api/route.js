293b42d297710eea69a2b3f6ede9f86abdeb5498
poom12345p <yhn-nhy@hotmail.com>
2019-10-15 03:46:13 +0700

    commit

--- routes/api/route.js
+++ routes/api/route.js
@@ -44,8 +44,8 @@ router.get('/data/products/:size&:vender',(req,res)=>{
 
   */
   console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
-  let size= req.params.size=='-'?'%':req.params.size;
-  let vendor= req.params.vender=='-'?'%':req.params.vender;
+  let size= req.params.size=='none'?'%':req.params.size;
+  let vendor= req.params.vender=='none'?'%':req.params.vender;
 
   db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" ORDER BY productName,productScale,productVendor `, { type: db.QueryTypes.SELECT})
   .then(result => {console.log(result);
@@ -63,9 +63,9 @@ router.get('/data/products/:size&:vender/name=:name',(req,res)=>{
   */
 
   console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
-  let size= req.params.size=='-'?'%':req.params.size;
-  let vendor= req.params.vender=='-'?'%':req.params.vender;
-  let name= req.params.name=='-'?'%':req.params.name;
+  let size= req.params.size=='none'?'%':req.params.size;
+  let vendor= req.params.vender=='none'?'%':req.params.vender;
+  let name= req.params.name=='0'?'%':req.params.name;
   db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" AND productName LIKE '%${name}%' ORDER BY productName,productScale,productVendor`, { type: db.QueryTypes.SELECT})
   .then(result => {console.log(result);
   res.send(result);
@@ -81,10 +81,10 @@ router.get('/data/products/:size&:vender/code=:code',(req,res)=>{
   */
   //console.log(`${req.params.size}`);
   console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
-  let size= req.params.size=='-'?'%':req.params.size;
-  let vendor= req.params.vender=='-'?'%':req.params.vender;
-  let name= req.params.code=='-'?'%':req.params.code;
-  db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" AND productCodeLIKE '%${name}%' ORDER BY productCode,productName,productScale,productVendor`, { type: db.QueryTypes.SELECT})
+  let size= req.params.size=='none'?'%':req.params.size;
+  let vendor= req.params.vender=='none'?'%':req.params.vender;
+  let code= req.params.code=='0'?'%':req.params.code;
+  db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" AND productCodeLIKE '%${code}%' ORDER BY productCode,productName,productScale,productVendor`, { type: db.QueryTypes.SELECT})
   .then(result => {console.log(result);
   res.send(result);
   })
@@ -110,9 +110,89 @@ router.get('/data/products/vendor',(req,res)=>{
   .catch(err => {console.log(err);});
 
 });
+//custommer
+router.get('/data/customers',(req,res)=>{
+  
+  db.query(`SELECT *
+  FROM customers
+  ORDER by customerName `, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
 
+router.get('/data/customers/name=:name',(req,res)=>{
+  
+  db.query(`SELECT *
+  FROM customers
+  WHERE customerName LIKE '${req.params.name}%';
+  ORDER by customerName  `, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
 
+router.get('/data/customers/number=:number',(req,res)=>{
+  
+  db.query(`SELECT *
+  FROM customers
+  WHERE customerNumber LIKE '${req.params.number}%';
+  ORDER by customerNumber,customerName  `, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
 
+//employees
+router.get('/data/employees/allTitle',(req,res)=>{
+  
+  db.query(`SELECT jobTitle
+  FROM employees
+  GROUP BY jobTitle`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+
+router.get('/data/employees/:title',(req,res)=>{
+  let t= req.params.title=='none'?'%':req.params.title;
+  db.query(`SELECT *
+  FROM employees
+  WHERE jobTitle LIKE '${t}'`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+
+router.get('/data/employees/:title/name=:name',(req,res)=>{
+  let t= req.params.title=='none'?'%':req.params.title;
+  let name= req.params.name=='0'?'%':req.params.name;
+  db.query(`SELECT *
+  FROM employees
+  WHERE jobTitle LIKE '${t}' AND employees.firstName||" "||employees.lastName LIKE '%${name}% '
+  ORDER BY employees.firstName,employees.lastName`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+router.get('/data/employees/:title/number=:number',(req,res)=>{
+  let t= req.params.title=='none'?'%':req.params.title;
+  let number= req.params.number=='0'?'%':req.params.number;
+  db.query(`SELECT *
+  FROM employees
+  WHERE jobTitle LIKE '${t}' AND employees.employeeNumber LIKE '%${number}% '
+  ORDER BY  employees.employeeNumber,employees.firstName,employees.lastName`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
 router.get('/i',(req,res)=>{
 
   db.query(`SELECT ${req,this.param,name} FROM  productlines`, { type: db.QueryTypes.SELECT})
