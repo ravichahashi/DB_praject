0858af86066f4472ce1cae4e2b57d20d45a0a93d
poom12345p <yhn-nhy@hotmail.com>
2019-10-14 22:55:35 +0700

    commit

--- routes/api/route.js
+++ routes/api/route.js
@@ -23,19 +23,113 @@ router.get('/orderlist',(req,res)=>{
 });
 
 
-router.get('/data/productlines/:name',(req,res)=>{
+router.get('/data/productlines',(req,res)=>{
   
-  db.query(`SELECT ${req,this.param,name} FROM  productlines`, { type: db.QueryTypes.SELECT})
+  db.query(`SELECT * FROM  productlines`, { type: db.QueryTypes.SELECT})
   .then(result => {console.log(result);
   res.send(result);
   })
   .catch(err => {console.log(err);});
 });
+//products sreach api
+router.get('/data/products/:size&:vender',(req,res)=>{
+  //console.log(`${req.params.size}`);
+  /*
+  ex. not select size and vendor
+  http://localhost:9000/data/products/-&-
+  ex. not select size only
+  http://localhost:9000/data/products/1:700&-
+   ex. not select size and vendor
+  http://localhost:9000/data/products/1:700&Min Lin Diecast
+
+  */
+  console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
+  let size= req.params.size=='-'?'%':req.params.size;
+  let vendor= req.params.vender=='-'?'%':req.params.vender;
+
+  db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" ORDER BY productName,productScale,productVendor `, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+
+router.get('/data/products/:size&:vender/name=:name',(req,res)=>{
+  //console.log(`${req.params.size}`);
+  /*
+  ex. 
+  http://localhost:9000/data/products/-&-/name=Alpine
+  ex. not select size only
+  */
+
+  console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
+  let size= req.params.size=='-'?'%':req.params.size;
+  let vendor= req.params.vender=='-'?'%':req.params.vender;
+  let name= req.params.name=='-'?'%':req.params.name;
+  db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" AND productName LIKE '%${name}%' ORDER BY productName,productScale,productVendor`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+
+router.get('/data/products/:size&:vender/code=:code',(req,res)=>{
+  /*
+  ex. 
+  http://localhost:9000/data/products/-&-/code=S12_1099
+  ex. not select size only
+  */
+  //console.log(`${req.params.size}`);
+  console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
+  let size= req.params.size=='-'?'%':req.params.size;
+  let vendor= req.params.vender=='-'?'%':req.params.vender;
+  let name= req.params.code=='-'?'%':req.params.code;
+  db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" AND productCodeLIKE '%${name}%' ORDER BY productCode,productName,productScale,productVendor`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+router.get('/data/products/:size&:vender/name&:name',(req,res)=>{
+  //console.log(`${req.params.size}`);
+  console.log(`${req.params.size}|${req.params.vender}|${req.params.name}`);
+  let size= req.params.size=='-'?'%':req.params.size;
+  let vendor= req.params.vender=='-'?'%':req.params.vender;
+  let name= req.params.name=='-'?'%':req.params.name;
+  db.query(`SELECT * FROM products WHERE productScale LIKE "${size}" AND productVendor LIKE "${vendor}" AND productName LIKE '%${name}%' ORDER BY productName `, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+});
+
+router.get('/data/products/size',(req,res)=>{
+  //console.log(`${req.params.size}`);
+  db.query(`SELECT productScale FROM products GROUP by productScale`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+
+});
+router.get('/data/products/vendor',(req,res)=>{
+  //console.log(`${req.params.size}`);
+  db.query(`SELECT productVendor FROM products GROUP by productVendor`, { type: db.QueryTypes.SELECT})
+  .then(result => {console.log(result);
+  res.send(result);
+  })
+  .catch(err => {console.log(err);});
+
+});
+
+
+
 router.get('/i',(req,res)=>{
 
-  db.query(`INSERT INTO productlines SELECT * FROM productlines`, { type: db.QueryTypes.INSERT})
+  db.query(`SELECT ${req,this.param,name} FROM  productlines`, { type: db.QueryTypes.SELECT})
   .then(result => {console.log(result);
-    res.sendFile(path.join(__dirname,`..`,`..`,`LOGIN.html`));})
+  res.send(result);
+  })
   .catch(err => {console.log(err);});
 });
     
